59174d8a48f6b2082b2f33bf4cff52f19cdf1b39
endemaster <endezhang2011@gmail.com>
2025-11-16 13:16:04 -0600

    Create whitelist.js

---
+++ whitelist.js
@@ -0,0 +1,19 @@
+export const whitelist = [
+  5357678423, // ende
+  78650586, // jasperjana
+  1127562842, // mrsigmaohio
+  7371804734, // monkey lee
+  6039702880, // twentyonepilots fan
+  6556325430, // tim
+  7505831865, // bart
+  5615559047, // daniel yu
+  1958152341, // philip
+  1675886817, // zhenya
+  5706761828, // sigma wu
+  7468269948, // luna
+  1313141417, // nate
+  6208934777, // jk
+  6486532366, // noah kim
+  1134533214, // charles
+  8404305590, // noahllee 
+];
