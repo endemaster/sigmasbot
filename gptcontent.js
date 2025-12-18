53e13caf0babb9cab89ffb19f31d53523c91257d
endemaster <endezhang2011@gmail.com>
2025-12-17 20:32:32 -0600

    Update gptcontent.js

--- gptcontent.js
+++ gptcontent.js
@@ -1,3 +1,23 @@
+export const whitelist = [
+  5357678423, // ende
+  78650586, // jasperjana
+  1127562842, // mrsigmaohio
+  7371804734, // monkey lee
+  6039702880, // twentyonepilots fan
+  6556325430, // tim
+  7505831865, // bart
+  5615559047, // daniel yu
+  1958152341, // philip
+  1675886817, // zhenya
+  5706761828, // sigma wu
+  7468269948, // luna
+  1313141417, // nate
+  6208934777, // jk
+  6486532366, // noah kim
+  1134533214, // charles
+  8404305590, // noahllee 
+];
+
 export const gptcontent = `Talk in a formal tone. Max completion tokens is 512, but try to keep it concise.
           Do not use any bold or other styling, as telegram doesnt support it.
 Here is background information about group members:
